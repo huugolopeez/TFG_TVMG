ebc4bed384f4dd32c12e4505ffba00627460d070
hugo <huugolopeez024@gmail.com>
2024-02-26 20:50:14 +0100

    Implementacion de dos botones en ExistAccount.dart para la navegacion entre login y register

--- lib/onBoarding/ExistAccount.dart
+++ lib/onBoarding/ExistAccount.dart
@@ -1,11 +1,60 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ExistAccount extends StatelessWidget {
 
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+  void onClickLogin() {
+
   }
 
+  void onClickRegister() {
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+        backgroundColor: const Color.fromRGBO(38, 41, 43, 1.0),
+        bottomNavigationBar: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+                padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                child: ConstrainedBox(
+                    constraints:
+                        const BoxConstraints.tightFor(width: 150, height: 50),
+                    child: ElevatedButton(
+                      onPressed: onClickLogin,
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              const Color.fromRGBO(38, 41, 43, 1.0)),
+                          side: MaterialStateProperty.all(const BorderSide(
+                              color: Color.fromRGBO(95, 122, 219, 1.0))),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(10)))),
+                      child: const Text('LOGIN'),
+                    ))),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+              child: ConstrainedBox(
+                constraints:
+                    const BoxConstraints.tightFor(width: 150, height: 50),
+                child: ElevatedButton(
+                  onPressed: onClickRegister,
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          const Color.fromRGBO(95, 122, 219, 1.0)),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)))),
+                  child: const Text('REGISTER'),
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:tfg_tvmg/TVMG.dart';
 
 import 'package:tfg_tvmg/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(TVMG());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
